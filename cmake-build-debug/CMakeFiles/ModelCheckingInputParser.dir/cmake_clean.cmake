15cc9ea2916eeb85ddb724ee172fcd13e544f624
pazhamalai <arunpazhamalai@gmail.com>
2021-07-07 23:58:43 +0530

    Added cmake file

---
+++ cmake-build-debug/CMakeFiles/ModelCheckingInputParser.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModelCheckingInputParser.dir/FileReaderUtils/fileReaderUtils.c.o"
+  "CMakeFiles/ModelCheckingInputParser.dir/FileReaderUtils/typeFinder.c.o"
+  "CMakeFiles/ModelCheckingInputParser.dir/FormulaParser/FormulaParser.c.o"
+  "CMakeFiles/ModelCheckingInputParser.dir/MCIP.c.o"
+  "CMakeFiles/ModelCheckingInputParser.dir/VariablesParser/variablesReader.c.o"
+  "CMakeFiles/ModelCheckingInputParser.dir/commonUtils.c.o"
+  "libModelCheckingInputParser.a"
+  "libModelCheckingInputParser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ModelCheckingInputParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
